99e9aaea2f9630b100c90dccacd0fa3e9e94f241
zfirdavs <firdavs792@gmail.com>
2023-03-12 18:31:28 +0500

    refactor: search command implementation

--- phonebook.go
+++ phonebook.go
@@ -3,18 +3,12 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"math/rand"
 	"os"
 	"path"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
-)
-
-const (
-	min = 0
-	max = 26
 )
 
 type Entry struct {
@@ -35,38 +29,19 @@ var (
 )
 
 func search(key string) *Entry {
-	for i, v := range data {
-		if v.Tel == key {
-			return &data[i]
-		}
+	i, ok := index[key]
+	if !ok {
+		return nil
 	}
-	return nil
+
+	data[i].LastAccess = strconv.FormatInt(time.Now().Unix(), 10)
+	return &data[i]
 }
 
 func list() {
 	for _, v := range data {
 		fmt.Println(v)
 	}
-}
-
-func random(min, max int) int {
-	return rand.Intn(max-min) + min
-}
-
-func getString(l int64) string {
-	startChar := "A"
-	temp := ""
-	var i int64 = 1
-	for {
-		myRand := random(min, max)
-		newChar := string(startChar[0] + byte(myRand))
-		temp = temp + newChar
-		if i == l {
-			break
-		}
-		i++
-	}
-	return temp
 }
 
 func readCSVFile(filepath string) error {
@@ -148,7 +123,7 @@ func initS(N, S, T string) *Entry {
 func insert(e *Entry) error {
 	// If tel is exist, return error
 	_, ok := index[e.Tel]
-	if !ok {
+	if ok {
 		return fmt.Errorf("%s already exists", e.Tel)
 	}
 	data = append(data, *e)
@@ -185,7 +160,6 @@ func main() {
 		fmt.Println("Creating", CSVFILE)
 		f, err := os.Create(CSVFILE)
 		if err != nil {
-			f.Close()
 			fmt.Println(err)
 			return
 		}
@@ -250,16 +224,23 @@ func main() {
 
 	case "search":
 		if len(args) != 3 {
-			fmt.Println("Usage: search Tel number")
+			fmt.Println("Usage: search Number")
 			return
 		}
 
-		result := search(args[2])
-		if result == nil {
-			fmt.Println("Entry not found:", args[2])
+		t := strings.ReplaceAll(args[2], "-", "")
+		if !matchTel(t) {
+			fmt.Println("Not a valid telephone number:", t)
 			return
 		}
-		fmt.Println(*result)
+
+		num := search(t)
+		if num == nil {
+			fmt.Println("Number not found:", args[2])
+			return
+		}
+
+		fmt.Println(*num)
 	case "list":
 		list()
 	default:
